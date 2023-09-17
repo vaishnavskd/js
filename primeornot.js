7748d939b24dc4d7798013070b7a1bfd5ac741c6
Vaishnav S <109717500+vaishnavskd@users.noreply.github.com>
2023-09-17 12:33:31 +0530

    Prime number checking executed successfully

--- primeornot.js
+++ primeornot.js
@@ -1,4 +1,22 @@
-function prime()
+function prime(number)
 {
-    
+    var flag=0;
+    for(let i=2;i<number;i++)
+    {
+        if(number%i==0)
+        {
+            flag=1;
+        }
+    }
+    if(flag==1)
+    {
+        console.log(number+" is not Prime");
+    }
+    else
+    {
+        console.log(number+" is Prime");
+    }
 }
+
+arr=[5,3,6,7,9,21,11];
+prime(arr[0]);
