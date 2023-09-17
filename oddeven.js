6f4b03a2551b1d4422aa5a86053a615290fe4578
Vaishnav S <109717500+vaishnavskd@users.noreply.github.com>
2023-09-17 12:17:07 +0530

    oddeven execution completed

---
+++ oddeven.js
@@ -0,0 +1,16 @@
+function oddeven(num)
+{
+    if(num%2==0)
+    {
+    console.log(num +" is Even");
+    }
+    else
+    {
+        console.log(num +" is Odd");
+    }
+}
+
+for(let i=0;i<=15;i++)
+{
+    oddeven(i);
+}
